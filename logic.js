7ed2921e1900a9f10f3367f90b3836935d17004c
beamerboy <luisjimenez10005@gmail.com>
2023-07-21 13:11:27 -0400

    logic for changing the colors working

--- logic.js
+++ logic.js
@@ -2,9 +2,16 @@ const primaryColors = [
     "yellow",
     "blue",
     "red",
+    "yellow",
 ];
 
-const secondaryColors = {
+const secondaryColors = [
+    "green",
+    "orange",
+    "purple",
+]
+
+const secondaryColorsCombinations = {
     //        combination
     "green": [primaryColors[0], primaryColors[1]],
     "orange": [primaryColors[0], primaryColors[2]],
@@ -15,3 +22,14 @@ const main = document.getElementById("main");
 const section = document.getElementById("section");
 const message = document.getElementById("message");
 const button = document.getElementById("button");
+
+currentIndex = 0;
+
+button.addEventListener("click", () => {
+    main.setAttribute("style", "background-color: "+secondaryColors[currentIndex]);
+    section.setAttribute("style", "background-color: "+primaryColors[currentIndex]);
+    message.setAttribute("style", "color: black");
+    button.innerText = primaryColors[currentIndex+1];
+
+    currentIndex === 2? currentIndex = 0: currentIndex++;
+})
