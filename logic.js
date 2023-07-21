1af275a721f558b43839b4a4b04785f86407a8d8
beamerboy <luisjimenez10005@gmail.com>
2023-07-21 12:53:44 -0400

    document's objects

--- logic.js
+++ logic.js
@@ -2,11 +2,16 @@ const primaryColors = [
     "yellow",
     "blue",
     "red",
-]
+];
 
 const secondaryColors = {
     //        combination
     "green": [primaryColors[0], primaryColors[1]],
     "orange": [primaryColors[0], primaryColors[2]],
     "purple": [primaryColors[1], primaryColors[2]]
-}
+};
+
+const main = document.getElementById("main");
+const section = document.getElementById("section");
+const message = document.getElementById("message");
+const button = document.getElementById("button");
