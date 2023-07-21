7839ba165d4d7777014f19d68d66310ee628dbb1
beamerboy <luisjimenez10005@gmail.com>
2023-07-21 12:38:02 -0400

    basic javascript

---
+++ logic.js
@@ -0,0 +1,12 @@
+const primaryColors = [
+    "yellow",
+    "blue",
+    "red",
+]
+
+const secondaryColors = {
+    //        combination
+    "green": [primaryColors[0], primaryColors[1]],
+    "orange": [primaryColors[0], primaryColors[2]],
+    "purple": [primaryColors[1], primaryColors[2]]
+}
